479e48fb635bbbdeb0cc528c9c3e5c56f4b73525
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-01-17 12:19:49 +0800

    edit

---
+++ src/app/motion/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { motion } from "motion/react";
+export default function Animation() {
+  return (
+    <div>
+      <div className="w-[500px] h-[500px] bg-grey flex flex-col items-center justify-center">
+        <p>hello world</p>
+        <button>click me</button>
+      </div>
+    </div>
+  );
+}
--- src/components/SecondSlide.jsx
+++ src/components/SecondSlide.jsx
@@ -26,74 +26,74 @@ export default function SecondSlide({
     setFormErrors((prev) => ({ ...prev, [name]: "" }));
     setFormValues((prev) => ({ ...prev, [name]: value }));
   };
-  // const handleClick = () => {
-  //   const { email, phoneNumber, password, confirmPassword } = formValues;
-  //   if (!email.trim()) {
-  //     setFormErrors((prev) => ({ ...prev, email: "Мэйл хаягаа оруулна уу" }));
-  //   } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-  //     setFormErrors((prev) => ({ ...prev, email: "Зөв мэйл хаяг оруулна уу" }));
-  //   }
-  // };
-  // if (!phoneNumber.trim()) {
-  //   setFormErrors((prev) => ({ ...prev, phoneNumber: "Тоо оруулна уу" }));
-  // } else if (!/^\+?\d{8}$/.test(phoneNumber)) {
-  //   setFormErrors((prev) => ({
-  //     ...prev,
-  //     phoneNumber: "8 оронтой тоо оруулна уу",
-  //   }));
-  // }
-
-  return (
-    <div className="w-[480px] h-[655px] p-8 rounded-lg border-1 bg-[#FFFFFF] flex flex-col justify-between m-auto gap-[8px]">
-      <div>
-        <Header />
-        <div className="gap-[12px] mt-[28px]">
-          <Input
-            label="Email"
-            placeholder="Your email"
-            error={""}
-            helperText="First name cannot contain special characters or numbers."
-            type="email"
-            name="email"
-            onChange={handleChange}
-          />
-          <Input
-            label="Phone number"
-            placeholder="Your phone number"
-            error={""}
-            helperTextText="Last name cannot contain special characters or numbers."
-            type="number"
-            name="phoneNumber"
-            onChange={handleChange}
-          />
-          <Input
-            label="Password"
-            placeholder="Your password"
-            error={""}
-            helperText="This username is already taken. Please choose another one."
-            type="password"
-            name="password"
-            onChange={handleChange}
-          />
-          <Input
-            label="Confirm password"
-            placeholder="Your confirm password"
-            error={""}
-            helperText="This username is already taken. Please choose another one."
-            type="password"
-            name="confirmPassword"
-            onChange={handleChange}
+  const handleClick = () => {
+    const { email, phoneNumber, password, confirmPassword } = formValues;
+    if (!email.trim()) {
+      setFormErrors((prev) => ({ ...prev, email: "Мэйл хаягаа оруулна уу" }));
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      setFormErrors((prev) => ({ ...prev, email: "Зөв мэйл хаяг оруулна уу" }));
+    }
+  };
+  if (!phoneNumber.trim()) {
+    setFormErrors((prev) => ({ ...prev, phoneNumber: "Тоо оруулна уу" }));
+  } else if (!/^\+?\d{8}$/.test(phoneNumber)) {
+    setFormErrors((prev) => ({
+      ...prev,
+      phoneNumber: "8 оронтой тоо оруулна уу",
+    }));
+  }
+  if (!password)
+    return (
+      <div className="w-[480px] h-[655px] p-8 rounded-lg border-1 bg-[#FFFFFF] flex flex-col justify-between m-auto gap-[8px]">
+        <div>
+          <Header />
+          <div className="gap-[12px] mt-[28px]">
+            <Input
+              label="Email"
+              placeholder="Your email"
+              error={""}
+              helperText="First name cannot contain special characters or numbers."
+              type="email"
+              name="email"
+              handleChange={handleChange}
+            />
+            <Input
+              label="Phone number"
+              placeholder="Your phone number"
+              error={""}
+              helperTextText="Last name cannot contain special characters or numbers."
+              type="number"
+              name="phoneNumber"
+              handleChange={handleChange}
+            />
+            <Input
+              label="Password"
+              placeholder="Your password"
+              error={""}
+              helperText="This username is already taken. Please choose another one."
+              type="password"
+              name="password"
+              handleChange={handleChange}
+            />
+            <Input
+              label="Confirm password"
+              placeholder="Your confirm password"
+              error={""}
+              helperText="This username is already taken. Please choose another one."
+              type="password"
+              name="confirmPassword"
+              handleChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <BackButton backCurrentSlides={backCurrentSlides} />
+          <Button
+            buttonText={currentSlide}
+            ChevronRight
+            setCurrentSlide={handleClick}
           />
         </div>
       </div>
-      <div className="flex gap-2">
-        <BackButton backCurrentSlides={backCurrentSlides} />
-        <Button
-          buttonText={currentSlide}
-          ChevronRight
-          // setCurrentSlide={handleClick}
-        />
-      </div>
-    </div>
-  );
+    );
 }
